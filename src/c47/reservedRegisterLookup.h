a44cd529d0740775deddcf5bd769802bbd786699
Jaco <>
2026-05-31 01:58:17 +0200

    remove last remnants of the 5 removed reserved vars by updating the lookup table

--- src/c47/reservedRegisterLookup.h
+++ src/c47/reservedRegisterLookup.h
@@ -1,5 +1,5 @@
-/* ANSI-C code produced by gperf version 3.3 */
-/* Command-line: gperf -C -G --null-strings -m 1000 -E -n -L ANSI-C -N lookupReservedVariableName -t  */
+/* ANSI-C code produced by gperf version 3.0.3 */
+/* Command-line: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/gperf -C -G --null-strings -m 1000 -E -n -L ANSI-C -N lookupReservedVariableName -t  */
 /* Computed positions: -k'1-3' */
 
 #if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
@@ -26,7 +26,7 @@
       && ('w' == 119) && ('x' == 120) && ('y' == 121) && ('z' == 122) \
       && ('{' == 123) && ('|' == 124) && ('}' == 125) && ('~' == 126))
 /* The character set is not based on ISO-646.  */
-#error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gperf@gnu.org>."
+#error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gnu-gperf@gnu.org>."
 #endif
 
 struct reservedRegister {
@@ -35,14 +35,14 @@ struct reservedRegister {
 };
 enum
   {
-    TOTAL_KEYWORDS = 22,
+    TOTAL_KEYWORDS = 17,
     MIN_WORD_LENGTH = 2,
     MAX_WORD_LENGTH = 6,
     MIN_HASH_VALUE = 0,
-    MAX_HASH_VALUE = 21
+    MAX_HASH_VALUE = 16
   };
 
-/* maximum key range = 22, duplicates = 0 */
+/* maximum key range = 17, duplicates = 0 */
 
 #ifdef __GNUC__
 __inline
@@ -52,55 +52,45 @@ inline
 #endif
 #endif
 static unsigned int
-hash (register const char *str, register size_t len)
+hash (register const char *str, register unsigned int len)
 {
   TO_QSPI static const unsigned char asso_values[] =
     {
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22,  9, 22,  8, 22, 22,
-      22, 22, 22, 22, 22, 22,  6,  8, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22,  5, 22,  3,  6,  6,
-       9, 10, 22,  4, 22, 22,  4,  7, 11, 22,
-       1, 22,  1,  4,  6, 22,  8, 22,  2,  0,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22,  1, 22,  0, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22,  0, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-      22, 22, 22, 22, 22, 22
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17,  6, 17, 17,
+      17, 17, 17, 17, 17, 17, 17,  5, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17,  3, 17,  5, 17,  6,
+       7,  6, 17,  5, 17, 17,  4,  5,  8, 17,
+       1, 17,  5, 17,  5, 17,  8, 17,  2,  0,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17,  1, 17,  0, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17,  0, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+      17, 17, 17, 17, 17, 17
     };
   register unsigned int hval = 0;
 
   switch(len) {
       default:
         hval += asso_values[(unsigned char)str[2]];
-#if (defined __cplusplus && (__cplusplus >= 201703L || (__cplusplus >= 201103L && defined __clang__ && __clang_major__ + (__clang_minor__ >= 9) > 3))) || (defined __STDC_VERSION__ && __STDC_VERSION__ >= 202000L && ((defined __GNUC__ && __GNUC__ >= 10) || (defined __clang__ && __clang_major__ >= 9)))
-      [[fallthrough]];
-#elif (defined __GNUC__ && __GNUC__ >= 7) || (defined __clang__ && __clang_major__ >= 10)
-      __attribute__ ((__fallthrough__));
-#endif
       /*FALLTHROUGH*/
       case 2:
         hval += asso_values[(unsigned char)str[1]];
-#if (defined __cplusplus && (__cplusplus >= 201703L || (__cplusplus >= 201103L && defined __clang__ && __clang_major__ + (__clang_minor__ >= 9) > 3))) || (defined __STDC_VERSION__ && __STDC_VERSION__ >= 202000L && ((defined __GNUC__ && __GNUC__ >= 10) || (defined __clang__ && __clang_major__ >= 9)))
-      [[fallthrough]];
-#elif (defined __GNUC__ && __GNUC__ >= 7) || (defined __clang__ && __clang_major__ >= 10)
-      __attribute__ ((__fallthrough__));
-#endif
       /*FALLTHROUGH*/
       case 1:
         hval += asso_values[(unsigned char)str[0]];
@@ -109,10 +99,6 @@ hash (register const char *str, register size_t len)
   return hval;
 }
 
-#if (defined __GNUC__ && __GNUC__ + (__GNUC_MINOR__ >= 6) > 4) || (defined __clang__ && __clang_major__ >= 3)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#endif
 TO_QSPI static const struct reservedRegister wordlist[] =
   {
     {"\241\223Y",RESERVED_VARIABLE_LY},
@@ -125,29 +111,21 @@ TO_QSPI static const struct reservedRegister wordlist[] =
     {"\241\221EST",RESERVED_VARIABLE_UEST},
     {"PPER/a",RESERVED_VARIABLE_PPERONA},
     {"PV",RESERVED_VARIABLE_PV},
-    {"CPER/a",RESERVED_VARIABLE_CPERONA},
-    {"ACC",RESERVED_VARIABLE_ACC},
-    {"REALDF",RESERVED_VARIABLE_REALDF},
     {"NPPER",RESERVED_VARIABLE_NPPER},
     {"PMT",RESERVED_VARIABLE_PMT},
-    {"ISM",RESERVED_VARIABLE_ISM},
+    {"CPER/a",RESERVED_VARIABLE_CPERONA},
+    {"ACC",RESERVED_VARIABLE_ACC},
     {"GRAMOD",RESERVED_VARIABLE_GRAMOD},
     {"FV",RESERVED_VARIABLE_FV},
-    {"ADM",RESERVED_VARIABLE_ADM},
-    {"D.MAX",RESERVED_VARIABLE_DENMAX},
-    {"I%/a",RESERVED_VARIABLE_IPONA},
-    {"#DEC",RESERVED_VARIABLE_NDEC}
+    {"I%/a",RESERVED_VARIABLE_IPONA}
   };
-#if (defined __GNUC__ && __GNUC__ + (__GNUC_MINOR__ >= 6) > 4) || (defined __clang__ && __clang_major__ >= 3)
-#pragma GCC diagnostic pop
-#endif
 
 const struct reservedRegister *
-lookupReservedVariableName (register const char *str, register size_t len)
+lookupReservedVariableName (register const char *str, register unsigned int len)
 {
   if(len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
     {
-      register unsigned int key = hash (str, len);
+      unsigned int key = hash (str, len);
 
       if(key <= MAX_HASH_VALUE)
         {
@@ -157,5 +135,5 @@ lookupReservedVariableName (register const char *str, register size_t len)
             return &wordlist[key];
         }
     }
-  return (struct reservedRegister *) 0;
+  return 0;
 }
--- src/c47/reservedRegisterLookupGenerator.c
+++ src/c47/reservedRegisterLookupGenerator.c
@@ -42,11 +42,9 @@ struct {
   {1, 'V',  0,   0,   0,   0,   0},
   {1, 'W',  0,   0,   0,   0,   0},
 #endif
-  {{3, 'A', 'D', 'M',  0,   0,   0 }, "RESERVED_VARIABLE_ADM" },
-  {{5, 'D', '.', 'M', 'A', 'X',  0 }, "RESERVED_VARIABLE_DENMAX" },
-  {{3, 'I', 'S', 'M',  0,   0,   0 }, "RESERVED_VARIABLE_ISM" },
-  {{6, 'R', 'E', 'A', 'L', 'D', 'F'}, "RESERVED_VARIABLE_REALDF" },
-  {{4, '#', 'D', 'E', 'C',  0,   0 }, "RESERVED_VARIABLE_NDEC" },
+
+//The gperf generator does not need a row per enum. It only needs rows for names that should resolve, therefore I remove the SPARE1-5 items. Return them when used again with valid names
+
   {{3, 'A', 'C', 'C',  0,   0,   0 }, "RESERVED_VARIABLE_ACC" },
   {{5, 161, 145, 'L', 'i', 'm',  0 }, "RESERVED_VARIABLE_ULIM" },
   {{5, 161, 147, 'L', 'i', 'm',  0 }, "RESERVED_VARIABLE_LLIM" },
