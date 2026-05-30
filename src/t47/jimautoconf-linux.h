5ed0a1a4a777ca442fdb8348cf965ec89306171e
Warren Young <tangentsoft@gmail.com>
2026-05-29 19:40:12 -0600

    Further Linux build fixes

--- src/t47/jimautoconf-linux.h
+++ src/t47/jimautoconf-linux.h
@@ -100,24 +100,14 @@
 #define jim_ext_aio 1
 #define jim_ext_array 1
 #define jim_ext_clock 1
-#define jim_ext_eventloop 1
 #define jim_ext_exec 1
 #define jim_ext_file 1
 #define jim_ext_glob 1
-#define jim_ext_history 1
-#define jim_ext_interp 1
-#define jim_ext_load 1
-#define jim_ext_namespace 1
-#define jim_ext_nshelper 1
-#define jim_ext_oo 1
 #define jim_ext_pack 1
 #define jim_ext_package 1
-#define jim_ext_posix 1
 #define jim_ext_readdir 1
 #define jim_ext_regexp 1
 #define jim_ext_signal 1
 #define jim_ext_stdlib 1
-#define jim_ext_syslog 1
 #define jim_ext_tclcompat 1
-#define jim_ext_tree 1
 #endif
