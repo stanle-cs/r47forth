cd9ea7bd7ea7845c9e146f3055bffcff006e771a
Jaco Mostert <jaco@cocoon-creations.com>
2026-06-28 13:18:37 +0200

    Fine tune HP42S glyphs to STD font and shift 0x2427 to 0x2404 and restore 0x2427; snap all fonts to their grids

--- src/c47/fonts.h
+++ src/c47/fonts.h
@@ -765,7 +765,7 @@
 #define STD_FOR_ALL                   "\xac\x6f" // was U+2200
 #define STD_RIGHT_DOUBLE_ARROW        "\xa1\xD2"    //JM
 #define STD_LEFT_RIGHT_DOUBLE_ARROW   "\xa1\xD4"    //JM
-#define STD_CHECKERED                 "\xa4\x27"
+#define STD_CHECKERED                 "\xa4\x04"
 #define STD_BLACK_RIGHT_TRIANGLE      "\xa5\xb6"
 #define STD_LF_PIC                    "\xa4\x0a"
 #define STD_ESC_PIC                   "\xa4\x1b"
