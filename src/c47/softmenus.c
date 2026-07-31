ecfeb0efe9da8513654a85f9e96039201653bb62
Jaco <>
2026-07-31 17:38:20 +0200

    Name spacing, menu re-arrangement

--- src/c47/softmenus.c
+++ src/c47/softmenus.c
@@ -202,9 +202,9 @@ TO_QSPI const int16_t menu_VECT[]        = {
 
 
 TO_QSPI const int16_t menu_VECCONV[]     = {
-                                             ITM_STKtoV2,                   ITM_V2toSTK,                ITM_VECtoREG,             ITM_REGtoVEC,          ITM_STKtoV3,                 ITM_V3toSTK,
-                                             ITM_NULL,                      ITM_NULL,                   ITM_stkexV4,              ITM_NULL,              ITM_STKtoV4,                 ITM_V4toSTK,
-                                             ITM_CPXtoV,                    ITM_VtoCPX,                 ITM_NULL,                 ITM_NULL,              ITM_3DPHYS,                  ITM_3DXYZ
+                                             ITM_STKtoV2,                   ITM_V2toSTK,                ITM_STKtoV3,              ITM_V3toSTK,           ITM_STKtoV4,                 ITM_V4toSTK,
+                                             ITM_stkexV2,                   ITM_NULL,                   ITM_stkexV3,              ITM_NULL,              ITM_stkexV4,                 ITM_NULL,              
+                                             ITM_CPXtoV,                    ITM_VtoCPX,                 ITM_VECtoREG,             ITM_REGtoVEC,          ITM_3DPHYS,                  ITM_3DXYZ
                                            };
 
 
